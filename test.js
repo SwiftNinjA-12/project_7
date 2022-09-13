6c57e8c14f5aac4cfae2e712f968a0b1c28c6018
Tom Verveckken <tverveckken@gmail.com>
2022-09-13 14:08:24 +0200

    loopdy loops

--- test.js
+++ test.js
@@ -4,9 +4,14 @@ const remainingChars = document.getElementById("remaining-chars");
 function updateRemainingChars(event) {
   const enteredTextLength = event.target.value.length;
   remainingChars.textContent = inputField.maxLength - enteredTextLength;
-  if (remainingChars.textContent < 11) {
+  if (remainingChars.textContent == 0) {
+    remainingChars.classList.add("error");
+    inputField.classList.add("error");
+  } else if (remainingChars.textContent <= 10) {
     remainingChars.classList.add("warning");
     inputField.classList.add("warning");
+    remainingChars.classList.remove("error");
+    inputField.classList.remove("error");
   } else {
     remainingChars.classList.remove("warning");
     inputField.classList.remove("warning");
